37f47d4dbe363687bbcce058bc464f8035614b16
Samantha Harper <149115727+Sharper93@users.noreply.github.com>
2025-04-15 18:41:02 +0000

    Adding props to function

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,17 +1,21 @@
 import "./App.css";
 
-function Header() {
+// passing props as an arg that is sent to headers function
+// header is child of app
+function Header(props) {
   return (
     <header>
-      <h1>Eve's Kitchen</h1>
+      <h1>{props.name}'s' Kitchen</h1>
+      <p>Copyright {props.year}</p>
     </header>
   );
 }
 
 function App() {
   return (
+    // passing properties for header function
     <div>
-      <Header />
+      <Header name="Sammi" year={new Date().getFullYear()}/>
       <main>
         <h2>We serve the most delicious food around</h2>
       </main>
