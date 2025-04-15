08f466c2e3ce415d832b9cf7f8c0b8882ea41d12
Samantha Harper <149115727+Sharper93@users.noreply.github.com>
2025-04-15 18:59:16 +0000

    use deconstructored props as fun args to be cleaner

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
-function Header(props) {
+// deconstruct props by {name, name}
+// allows props. to be removed from each call of the properties name
+function Header({name, year}) {
   return (
     <header>
-      <h1>{props.name}'s Kitchen</h1>
-      <p>Copyright {props.year}</p>
+      <h1>{name}'s Kitchen</h1>
+      <p>Copyright {year}</p>
     </header>
   );
 }
@@ -12,7 +14,7 @@ function Header(props) {
 function App() {
   return (
     <div>
-      <Header name="Alex" year={new Date().getFullYear()} />
+      <Header name="Sammi" year={new Date().getFullYear()} />
       <main>
         <h2>We serve the most delicious food around</h2>
       </main>
