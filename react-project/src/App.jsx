ba12ca6003094d5cc44c0d0de731b961c4c9cad1
Samantha Harper <149115727+Sharper93@users.noreply.github.com>
2025-04-16 17:15:36 +0000

    Adding H2 to site and wrapping main in jsx fragment <> </>

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -23,30 +23,35 @@ const dishObjects = items.map((dish, i) => ({
 
 function Main({ dishes }) {
   return (
-    <main>
-      <img
-        src={chef}
-        height={200}
-        alt="A photo of a smiling chef owner"
-      />
-      <ul>
-        {dishes.map((dish) => (
-          <li
-            key={dish.id}
-            style={{ listStyleType: "none" }}
-          >
-            {dish.title}
-          </li>
-        ))}
-      </ul>
-    </main>
+    <>
+      <div>
+        <h2>Welcome to this beautiful restaurant!</h2>
+      </div>
+      <main>
+        <img
+          src={chef}
+          height={200}
+          alt="A photo of a smiling chef owner"
+        />
+        <ul>
+          {dishes.map((dish) => (
+            <li
+              key={dish.id}
+              style={{ listStyleType: "none" }}
+            >
+              {dish.title}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
 function App() {
   return (
     <div>
-      <Header name="Alex" year={new Date().getFullYear()} />
+      <Header name="Sammi" year={new Date().getFullYear()} />
       <Main dishes={dishObjects} />
     </div>
   );
